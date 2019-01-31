06edd4ab9da3447b51a559841f60b40bc4725f84
zfeng7527 <zfeng7527@bths.edu>
2019-01-31 08:32:51 -0500

    Working

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,7 +6,19 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
-
+        int[] arr3 = new int[arr1.length+arr2.length];
+        int i =0,j=0,k=0;
+        while(k<(arr1.length+arr2.length)){
+            if( i<arr1.length && arr1[i]<arr2[j]){
+                arr3[k]=arr1[i];
+                i++;
+            }
+            else{
+                arr3[k]=arr2[j];
+                j++;
+            }
+            k++;
+        }
+        return arr3;
     }
-
 }
