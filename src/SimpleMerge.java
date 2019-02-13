5e5a9cf11df0252a50b0429c4f9ac747ad41fd4f
zfeng7527 <zfeng7527@bths.edu>
2019-02-12 22:30:51 -0500

    Idk

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -21,4 +21,57 @@ public class SimpleMerge {
         }
         return arr3;
     }
+
+    public void mergeSort(int[] arr){
+        int n = arr.length;
+        int[] temp= new int[n];
+        mergeSortHelper(arr,0,n-1,temp);
+    }
+
+    public void mergeSortHelper(int[] arr, int left,int right,int[] temp){
+        if(left<right){
+            int mid = (left+right)/2;
+            mergeSortHelper(arr,left,mid,temp);
+            mergeSortHelper(arr,mid+1,right,temp);
+            merge(arr,left,mid,right,temp);
+        }
+    }
+
+    public void merge(int[] arr, int left,int mid, int right, int[] temp){
+            int i = left ,j = mid+1,k=left;
+
+            while(i <= mid && j <= right)
+            {
+                if(arr[i] < arr[j])
+                {
+                    temp[k] = arr[i];
+                    i++;
+                }
+                else
+                {
+                    temp[k] = arr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while(i <= mid)
+            {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+
+            while(j<= right) {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+
+            for(k= left; k<= right; k++)
+            {
+                arr[k] = temp[k];
+            }
+        }
+
 }
